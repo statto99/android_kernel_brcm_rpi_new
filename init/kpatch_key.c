47799a3467bea3db59605cbf26387848e2f4e94e
statto99 <185983635+statto99@users.noreply.github.com>
2026-04-04 00:26:35 +0100

    Update kpatch_key.c

--- init/kpatch_key.c
+++ init/kpatch_key.c
@@ -1,3 +1,6 @@
 #include <linux/init.h>
+
 char *kpatch_superkey = "Test123";
 int kpatch_has_key = 1;
+// Add this line to satisfy any other missing references
+void kpatch_hook_exec(void *bprm) { }
