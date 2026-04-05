116d9afd091c50ec695f57bf58e2d99ae06f4c16
statto99 <185983635+statto99@users.noreply.github.com>
2026-04-05 20:15:28 +0100

    Update hmac_capture.c

--- drivers/hmac_capture/hmac_capture.c
+++ drivers/hmac_capture/hmac_capture.c
@@ -13,7 +13,7 @@
 #include <linux/workqueue.h>
 #include <asm/ptrace.h>
 
-#define HMAC_ENTRY  0x143ac
+#define HMAC_ENTRY  0x4c7d4
 #define ANON_SIZE   0x78000
 #define TARGET_COMM "ar.tvplayer.tv"
 
