13187a3cc00fad94c94fa599135b617d7dbf4bed
statto99 <185983635+statto99@users.noreply.github.com>
2026-04-05 17:45:10 +0100

    Update hmac_capture.c

--- drivers/hmac_capture/hmac_capture.c
+++ drivers/hmac_capture/hmac_capture.c
@@ -7,8 +7,6 @@
 #define TARGET_COMM "ar.tvplayer.tv"
 #define ANON_SIZE   0x78000
 
-static int fired = 0;
-
 static int mmap_handler(struct kprobe *kp, struct pt_regs *regs)
 {
     struct pt_regs *uregs;
@@ -21,15 +19,10 @@ static int mmap_handler(struct kprobe *kp, struct pt_regs *regs)
     prot   = uregs->regs[2];
     flags  = uregs->regs[3];
 
-    /* Log all mmaps so we can see what sizes/prots are used */
-    if (fired < 30) {
-        fired++;
-        pr_info("hmac_capture: mmap pid=%d len=0x%lx prot=0x%lx flags=0x%lx\n",
+    if (prot & 0x4) {
+        pr_info("hmac_capture: exec mmap pid=%d len=0x%lx prot=0x%lx flags=0x%lx\n",
                 current->pid, len, prot, flags);
     }
-
-    return 0;
-}
 
 static struct kprobe kp = {
     .symbol_name = "__arm64_sys_mmap",
